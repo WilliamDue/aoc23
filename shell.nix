6a2b6134b79150553baab7a5c08b5da4c6dc18da
Due <williamhenrichdue@gmail.com>
2023-12-01 10:22:47 +0100

    removed some junk

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixos-unstable> { config = { allowUnfree = true; }; } }:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+    dotnet-sdk_7
+    fsautocomplete
+  ];
+}
